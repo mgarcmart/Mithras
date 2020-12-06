b26c91f48fb1b40c9d54e342cfa3d3eb90ba269b
Miguel Garcia Martin <mgm@kk-itsystems.de>
2020-12-06 14:16:16 +0100

    Added OpenGL to the window
    	new file:   .gitignore
    	modified:   go.mod
    	modified:   go.sum
    	modified:   main.go

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"log"
 	"runtime"
 
-	"github.com/go-gl/gl/v4.6-core/gl"
+	"github.com/go-gl/gl/v4.3-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
@@ -17,20 +18,35 @@ func main() {
 	defer glfw.Terminate()
 
 	window.MakeContextCurrent()
+	//program := initGl()
+	if err := gl.Init(); err != nil {
+		log.Fatal(err.Error())
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version ", version)
+
+	window.SetFramebufferSizeCallback(framebufferSizeCallback)
+	gl.ClearColor(0.1, 0.1, 0.5, 0.1)
 
 	for !window.ShouldClose() {
 		// Here comes the opengl stuff
 		processInput(window)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
 }
 
-func initGl() {
+func initGl() uint32 {
 
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
+	prog := gl.CreateProgram()
+	gl.LinkProgram(prog)
+	return prog
+}
+
+func framebufferSizeCallback(w *glfw.Window, width int, height int) {
+	gl.Viewport(0, 0, int32(width), int32(height))
 }
 
 func processInput(w *glfw.Window) {
@@ -48,9 +64,11 @@ func initGlfw() *glfw.Window {
 		panic(err)
 	}
 
-	glfw.WindowHint(glfw.ContextVersionMajor, 3)
-	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 6)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
 	window, err := glfw.CreateWindow(640, 480, "Mithra Engine", nil, nil)
 	if err != nil {
